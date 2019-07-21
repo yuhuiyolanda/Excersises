b0f3c311a6ade196c164ba1b9051404c7e4e83e4
yuhuiyolanda <1176794277@qq.com>
2019-07-21 20:19:36 +0800

    lodash

--- lodash/yuhuiyolanda-lodash.js
+++ lodash/yuhuiyolanda-lodash.js
@@ -2,4 +2,152 @@ var yuhuiyolanda = {
     compact: function(ary) {
       return ary.filter(it => it)
     },
-  }
+    //keypoint for chunk: 如果 end 大于数组的长度，slice 也会一直提取到原数组末尾。
+    chunk: function(arr, size = 1) {
+      var ans = []       
+    for(var i = 0;i < arr.length;i+=size){
+     var item = arr.slice(i,i + size)
+     ans.push(item)
+     }
+     return ans     
+    },
+    difference: function(arr,...values){
+      var toExclude = [].concat(...values)
+      return arr.filter(item => !toExclude.includes(item)) 
+    },
+   //keypoint for drop: 如果 begin 大于原数组的长度，则会返回空数组。
+    drop: function(arr,n){
+      return arr.slice(n)
+    },
+    dropright:  function(arr,n){
+      if(n >= arr.length){
+        return []
+      }else{
+        return arr.slice(0,arr.length - n)
+      }
+    },
+    fill: function(array,value,start = 0,end = array.length){
+      for(var i = start;i < end;i++){
+        array[i] = value 
+      }
+      return array        
+    },
+ 
+    flatten: function(array){
+       return [].concat(...array)
+    },
+
+    flattenDeep: function(array){
+      var result = []
+        for(var item of array){
+          if(Array.isArray(item)){
+              result.push(...flattenDeep(item))
+          }else{
+            result.push(item)
+          }
+        }
+        return result 
+    },
+
+    flattenDepth:function(array,depth = 1){
+      var result = []
+      for(var item of array){
+        if(Array.isArray(item)){
+          var flattedItem = flattenDepth(item, depth - 1)
+          result.push(...flattedItem)
+        }else{
+          result.push(item)
+        }
+      }
+      return result 
+    },
+    head:function(array){
+        return array[0]
+      },
+    indexOf:function(array,value,fromIndex = 0){
+        for(i = fromIndex;i < array.length;i++){
+          if(array[i] === value){
+            return i  
+          }
+        }
+        return -1
+    },
+    initial:function(array){
+      array.pop()
+      return array 
+    },
+    interSection:function(...arys){
+      for(var i = 1;i < arguments.length;i++){
+       arguments[0] = arguments[0].filter(it => arguments[i].includes(it))
+      }
+      return arguments[0]       
+    },
+    last:function(array){
+      return array[array.length - 1]
+    },
+   concat: function(array,...value){
+     var result = []
+     for(var item of arguments){
+       if(Array.isArray(item)){
+         result.push(...item)
+       }else{
+         result.push(item)
+       }
+     }
+     return result 
+    },
+    identity: function(value){
+      return value 
+    },
+    isArray: function(value){
+      return Object.prototype.toString.call(value) === "[Object,Array]"
+    },
+    isArguments: function(value){
+      return Object.prototype.toString.call(value) === "[Object,Arguments]"
+    },
+    isBoolean: function(value){
+      return Object.prototype.toString.call(value) === "[Object,Boolean]"
+    },
+    isDate: function(value){
+      return Object.prototype.toString.call(value) === "[Object,Date]"
+    },
+    isFunction: function(value){
+      return Object.prototype.toString.call(value) === "[Object,Function]"
+    },
+    isObject:function(value){
+      return value instanceof Object 
+    },
+    isRegExp:function(value){
+      return Object.prototype.toString.call(value) === "[Object,RegExp]"
+    },
+    isString: function(value){
+      return  typeof value === 'string'
+    },
+    isUndefined: function(value){
+      return  typeof value === 'undefined'
+    },
+    fromPairs:function(array){
+      var result = {}
+      for(var item of array){
+        result[item[0]] = item[1]
+      }
+      return result 
+    },
+    reverse:function(array){
+      var res = []
+      for(var i = array.length - 1;i >= 0;i--){
+        res.push(array[i])
+      }
+      return res 
+    },
+    negate:function(f){
+      return function(...args){
+        return !f(...args)
+      }
+    },
+   
+
+  };
+   
+
+
